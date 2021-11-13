9cb01875510c566cce4fcfccebd7fdfa3421eb33
Josh <josh@josh>
2021-11-12 19:11:30 -0600

    more validation added. untested

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -14,29 +14,68 @@
             $this->validateLastName($ln);
             $this->validateUEmails($em, $em2);
             $this->validatePasswords($pw, $pw2);
+
+            if(empty($this->errorArray)) {
+                //insert into DB
+                return true;
+            } else {
+                return false;
+            }
         }
 
 
         private function validateUsername($un) {
             if(strlen($un) > 25 || strlen($un) < 5) {
-
+                array_push($this->errorArray, "Your username must be between 5 and 25 characters");
+                return;
             }
+
+            //TODO: check if username exists
         }
 
         private function validateFirstName($fn) {
-            
+            if(strlen($fn) > 25 || strlen($fn) < 2) {
+                array_push($this->errorArray, "Your first name must be between 2 and 25 characters");
+                return;
+            }
         }
 
         private function validateLastName($ln) {
-            
+            if(strlen($ln) > 25 || strlen($ln) < 2) {
+                array_push($this->errorArray, "Your last name must be between 2 and 25 characters");
+                return;
+            }
         }
 
         private function validateUEmails($em, $em2) {
-            
+            if($em != $em2) {
+                array_push($this->errorArray, "Your emails do not match");
+                return;
+            }
+
+            if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
+                array_push($this->errorArray, "email is invalid");
+                return;
+            }
+
+            //TODO: check that username has not already been used
         }
 
         private function validatePasswords($pw, $pw2) {
-            
+            if($pw != $pw2) {
+                array_push($this->errorArray, "Your passwords do not match");
+                return;
+            }
+
+            if(preg_match('/[^A-Za-z0-9]/', $pw)) {
+                array_push($this->errorArray, "Your password can only contain letters and numbers");
+                return;
+            }
+
+            if(strlen($pw) > 30 || strlen($pw) < 5) {
+                array_push($this->errorArray, "Your password must be between 5 and 30 characters");
+                return;
+            }
         }
 
 
