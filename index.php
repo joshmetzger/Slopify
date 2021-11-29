c7f4719d14792b7e8216e86c80d4286c0940cae8
Josh <josh@josh>
2021-11-29 16:00:12 -0600

    albums showing on index page

--- index.php
+++ index.php
@@ -22,11 +22,13 @@
 
         while($row = mysqli_fetch_array($albumQuery)) {
             echo "<div class='gridViewItem'>
-                <img src='" . $row['artworkPath'] . "'>
-                <div class='gridViewInfo'>"
-                 . $row['title'] . 
-                "</div>
-            </div>";
+                    <a href='album.php?id=" . $row['id'] . "'>
+                        <img src='" . $row['artworkPath'] . "'>
+                        <div class='gridViewInfo'>"
+                            . $row['title'] . 
+                        "</div>
+                    </a>
+                </div>";
         }
     ?>
 </div>
