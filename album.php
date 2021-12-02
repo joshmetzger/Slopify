382d312c3126abfe7f1f42e876f1fc8029c690ff
Josh <josh@josh>
2021-12-02 16:52:48 -0600

    album page front mostly complete 12-2-21

--- album.php
+++ album.php
@@ -26,4 +26,46 @@ include("includes/header.php");
     </div>
 </div>
 
+<div class="trackListContainer">
+    <ul class="tracklist">
+
+        <?php
+        $songIdArray = $album->getSongIds();
+
+        $i = 1;
+        foreach($songIdArray as $songId) {
+            
+            $albumSong = new Song($con, $songId);
+            $albumArtist = $albumSong->getArtist();
+
+            echo "<li class='tracklistRow'>
+
+                <div class='trackCount'>
+                    <img class='play' src='assets/images/icons/play-white.png'>
+                    <span class='trackNumber'>$i</span>
+                </div>
+
+                <div class='trackInfo'>
+                    <span class='trackName'>" . $albumSong->getTitle() . "</span>
+                    <span class='artistName'>" . $albumArtist->getName() . "</span>
+                </div>
+
+                <div class='trackOptions'>
+                    <img class='optionsButtons' src='assets/images/icons/more.png'>
+                </div>
+
+                <div class='trackDuration'>
+                    <span class='duration'>" . $albumSong->getDuration() . "</span>
+                </div>
+
+            </li>";
+
+            $i = $i + 1;
+        }
+
+        ?>
+
+    </ul>
+</div>
+
 <?php include("includes/footer.php"); ?>
