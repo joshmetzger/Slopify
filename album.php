0a7e59cbf8a9bb43a31adc25603a5ebadd210f26
Josh <josh@josh>
2021-12-02 10:20:42 -0600

    single album page view and backend 12-2-21

--- album.php
+++ album.php
@@ -8,18 +8,22 @@ include("includes/header.php");
         header("Location: index.php");
     }
 
-    $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
-    $album = mysqli_fetch_array($albumQuery);
+    $album = new Album($con, $albumId);
 
-    
-    $artist = new Artist($con, $album['artist']);
-
-    echo $album['title'] . "<br>";
-    echo $artist->getName();
+    $artist = $album->getArtist();
 
 ?>
 
+<div class="entityInfo">
+    <div class="leftSection">
+        <img src="<?php echo $album->getArtworkPath(); ?>" alt="">
+    </div>
 
-
+    <div class="rightSection">
+        <h2><?php echo $album->getTitle(); ?></h2>
+        <p><?php echo $artist->getName(); ?></p>
+        <p><?php echo $album->getNumberOfSongs(); ?> songs</p>
+    </div>
+</div>
 
 <?php include("includes/footer.php"); ?>
