8fa67abd024743a08f3ef30c697462d3210c2f25
Josh <josh@josh>
2021-12-09 08:10:54 -0600

    song duration shows on right of progress bar 12-9-21

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
 var currentPlaylist = [];
 var audioElement;
 
+function formatTime(seconds) {
+    var time = Math.round(seconds);
+    var minutes = Math.floor(time / 60);
+    var seconds = time - (minutes * 60);
+
+    var extraZero = (seconds < 10) ? "0" : "";
+
+    return minutes + ":" + extraZero + seconds;
+}
 
 function Audio() {
 
     this.currentlyPlaying;
     this.audio = document.createElement('audio');
+
+    this.audio.addEventListener("canplay", function() {
+        var duration = formatTime(this.duration);
+        $(".progressTime.remaining").text(duration);
+    });
 
     this.setTrack = function(track) {
         this.currentlyPlaying = track;
